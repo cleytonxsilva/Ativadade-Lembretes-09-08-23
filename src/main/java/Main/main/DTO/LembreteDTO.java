88ff02d9c07ed1823095ab9fa3318804ff1f2431
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-09-06 19:21:37 -0300

    added mockito and tests

--- src/main/java/Main/main/DTO/LembreteDTO.java
+++ src/main/java/Main/main/DTO/LembreteDTO.java
@@ -12,9 +12,9 @@ public class LembreteDTO {
 
     private Long id;
     private String mensagem;
-    private Pessoa pessoa;
+    private PessoaDTO pessoa;
 
-    public LembreteDTO(Long id, String mensagem, PessoaDTO pessoaDTO) {
+    public LembreteDTO(Long id, String mensagem, PessoaDTO pessoa) {
         this.id = id;
         this.mensagem = mensagem;
         this.pessoa = pessoa;
--- src/test/java/Main/main/LembreteApplicationTests.java
+++ src/test/java/Main/main/LembreteApplicationTests.java
@@ -1,13 +1,35 @@
 package Main.main;
 
+import Main.main.Controller.PessoaController;
+import Main.main.Entity.Pessoa;
+import Main.main.Repository.PessoaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 @SpringBootTest
 class LembreteApplicationTests {
 
+	@MockBean
+	PessoaRepository pessoaRepository;
+	private  final PessoaController pessoaController = new PessoaController();
+
+	@BeforeEach
+	void injectData(){
+		Optional<Pessoa> pessoa = Optional.of(new Pessoa(1L, "Cleyton"));
+		Long id = 1L;
+		Mockito.when(pessoaRepository.findById(id)).thenReturn(pessoa);
+	}
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Caso de Uso FindById Classe Pessoa")
+	void testFindByIdPessoaTest(){
+
 	}
 
 }
